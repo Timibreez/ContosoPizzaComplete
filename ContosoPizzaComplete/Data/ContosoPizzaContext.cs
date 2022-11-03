5539ec121b9cba13caa0770e3b9376e0b3735f9b
Timibreez <ffftimi@gmail.com>
2022-11-03 11:43:43 +0100

    cleaning code

--- ContosoPizzaComplete/Data/ContosoPizzaContext.cs
+++ ContosoPizzaComplete/Data/ContosoPizzaContext.cs
@@ -3,9 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using ContosoPizzaComplete.Models;
-using System.Configuration;
 using Microsoft.Extensions.Configuration;
 
 namespace ContosoPizzaComplete.Data
