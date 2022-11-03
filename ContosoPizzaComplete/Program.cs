5b8a942eed9b3e9d6c38f320a924d6dfdae90317
Timibreez <ffftimi@gmail.com>
2022-11-03 13:03:46 +0100

    added products

--- ContosoPizzaComplete/Program.cs
+++ ContosoPizzaComplete/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ContosoPizzaComplete.Data;
+using ContosoPizzaComplete.Models;
+
+using ContosoPizzaContext context = new ContosoPizzaContext();
+
+var products = context.Products
+    .Where(p => p.Price > 500)
+    .OrderBy(p => p.Name);
+
+foreach (Product p in products)
+{
+    Console.WriteLine($"Id: {p.Id}");
+    Console.WriteLine($"Name: {p.Name}");
+    Console.WriteLine($"Price: {p.Price}");
+    Console.WriteLine(new string('-', 20));
+}
